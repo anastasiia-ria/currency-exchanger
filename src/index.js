c22c3f4c7f073df603bc36640a2969fbd3f711aa
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-11 10:03:04 -0800

    store and get rates from sessionStorage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import $ from "jquery";
 import "./css/styles.css";
 import Currency from "./js/currency.js";
 
-let ratesArray = [];
-
 async function makeApiCall() {
   const response = await Currency.getCurrency();
   getElements(response);
@@ -12,14 +10,10 @@ async function makeApiCall() {
 function getElements(response) {
   if (response) {
     const conversionRates = response.conversion_rates;
-    let i = 0;
     for (let key in conversionRates) {
-      $("#currency-from").append(`<option name="currency-from" value="${i} id="${key}">${key}</option>`);
-      $("#currency-to").append(`<option name="currency-to" value="${i} id="${key}">${key}</option>`);
-      i++;
-    }
-    for (let value of Object.values(conversionRates)) {
-      ratesArray.push(value);
+      $("#currency-from").append(`<option name="currency-from" value="${key}">${key}</option>`);
+      $("#currency-to").append(`<option name="currency-to" value="${key}">${key}</option>`);
+      sessionStorage.setItem(key, conversionRates[key]);
     }
   } else {
     $(".showErrors").text(`There was an error: ${response}`);
@@ -31,23 +25,18 @@ $(document).ready(function () {
   $("form#converter").submit(function (event) {
     event.preventDefault();
     const valueFrom = parseFloat($("input#value").val());
-    const currencyFromIndex = parseInt($("option[name='currency-from']:selected").val());
-    const currencyToIndex = parseInt($("option[name='currency-to']:selected").val());
-    const valueTo = Math.round(((valueFrom * ratesArray[currencyToIndex]) / ratesArray[currencyFromIndex]) * 100) / 100;
+    const currencyFrom = $("option[name='currency-from']:selected").val();
+    const currencyTo = $("option[name='currency-to']:selected").val();
+    const rateTo = sessionStorage.getItem(currencyTo);
+    const rateFrom = sessionStorage.getItem(currencyFrom);
+    const rate = rateTo / rateFrom;
+    const valueTo = valueFrom * rate;
 
-    const currencyFrom = $("option[name='currency-from']:selected").attr("id");
-    const currencyTo = $("option[name='currency-to']:selected").attr("id");
-
-    console.log(valueFrom);
-    console.log(ratesArray[currencyFromIndex]);
-    console.log(ratesArray[currencyToIndex]);
-    console.log(valueTo);
-    console.log(currencyFrom);
-    console.log(currencyTo);
-
-    $("#res-amount-from").html(valueFrom);
-    $("#res-currency-from").html(currencyFrom);
-    $("#res-amount-to").html(valueTo);
-    $("#res-currency-to").html(currencyTo);
+    $(".res-amount-from").html(valueFrom);
+    $(".res-currency-from").html(currencyFrom);
+    $(".res-amount-to").html(valueTo);
+    $(".res-currency-to").html(currencyTo);
+    $(".rate-to").html(rate);
+    $(".rate-from").html(1 / rate);
   });
 });
